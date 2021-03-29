6560c0a965c5592b2d99cd2ac500e6a3cc77853f
nitgi <78422040+nitgi@users.noreply.github.com>
2021-03-29 21:14:48 +0200

    Create count_racisme.sh

---
+++ count_racisme.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Author: Niels Top
+# Filename: count_racisme.sh
+# Date: 25-03-2021
+
+# This script is used to collect data for the paper: 'how the murder of George Floyd affected racism on social media' (Top, 2020)
+# Running the script will take approximately 5 hours
+# For further instructions on how to use the script consult the README.md file found at: 
+
+
+function get_racisme_count {
+	zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -i 'racisme' | wc -l
+}
+
+function get_non_racisme_count {
+	zless *.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | grep -iv 'racisme' | wc -l
+}
+
+echo This might take a while! Plenty of time to relax or work on something else.
+echo When all data from a month is processed you will be notified with a message in the shell.
+echo
+echo
+
+# 01-2020
+cd /net/corpora/twitter2/Tweets/2020/01
+echo Started processing tweets posted in January 2020...
+get_racisme_count
+echo
+echo tweets that do contain the word 'racisme'
+echo 
+get_non_racisme_count
+echo tweets that do not contain the word 'racisme'
+echo
+echo Collected all data from January
+echo
+echo
+
+# 06-2020
+cd /net/corpora/twitter2/Tweets/2020/06
+echo Started processing tweets posted in June 2020...
+get_racisme_count
+echo tweets that do contain the word 'racisme'
+echo 
+get_non_racisme_count
+echo tweets that do not contain the word 'racisme'
+echo
+echo Collected all data from June
+echo
+echo
+
+# 09-2020
+cd /net/corpora/twitter2/Tweets/2020/09
+echo Started processing tweets posted in September 2020...
+get_racisme_count
+echo tweets that do contain the word 'racisme'
+echo 
+get_non_racisme_count
+echo tweets that do not contain the word 'racisme'
+echo
+echo Collected all data from September
+echo
+echo
+
+# Finished
+echo Finished collecting the data
